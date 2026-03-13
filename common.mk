b370ca35efb65581f9ab70e56cd9285caf770e08
amit-0i <6329amit@gmail.com>
2026-03-13 22:23:38 +0530

    sm8350-common: dolby: initial changes for dolby
    Signed-off-by: amit-0i <amit0i+github@proton.me>

--- common.mk
+++ common.mk
@@ -120,6 +120,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
