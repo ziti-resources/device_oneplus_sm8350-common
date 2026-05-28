199fd8c972130edc310ca92f5289791f8684b98b
amit-0i <amit0i+github@proton.me>
2026-05-28 15:45:53 +0530

    sm8350-common: move few kernel scripts to device tree
    Signed-off-by: amit-0i <amit0i+github@proton.me>

--- common.mk
+++ common.mk
@@ -173,6 +173,8 @@ PRODUCT_PACKAGES += \
     fstab.default \
     fstab.default.vendor_ramdisk \
     init.class_main.sh \
+    init.kernel.post_boot-lahaina.sh \
+    init.kernel.post_boot-yupik.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
@@ -183,6 +185,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.qti.kernel.rc \
     init.target.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc
