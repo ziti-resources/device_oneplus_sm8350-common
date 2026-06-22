7a440f339dd2a763b067a915dddfe73cf1230275
Michael Bestas <mkbestas@lineageos.org>
2026-06-22 03:46:06 +0300

    sm8350-common: Update WFD system blobs from marvel_g A171WEH.20
    Change-Id: I733c2bf342175474ce7f02d467a17ded6f0a8760
    Signed-off-by: amit-0i <amit0i@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -66,6 +66,10 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('/my_product', '/product'),
     'system_ext/bin/horae': blob_fixup()
         .replace_needed('libprotobuf-cpp-lite.so', 'libprotobuf-cpp-lite-21.7.so'),
+    'system/framework/WfdCommon.jar': blob_fixup()
+        .apktool_patch('blob-patches/WfdCommon.patch'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/bin/init.kernel.post_boot-lahaina.sh': blob_fixup()
         .regex_replace('uag', 'schedutil'),
     ('vendor/etc/media_lahaina/video_system_specs.json', 'vendor/etc/media_yupik_v1/video_system_specs.json'): blob_fixup()
